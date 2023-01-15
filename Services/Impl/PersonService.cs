db520d3c1a435feb084e5226d904f3853a831dc5
ginger228 <crystal.swipe@gmail.com>
2023-01-15 21:40:51 +0200

    Removed redundant imports

--- Services/Impl/PersonService.cs
+++ Services/Impl/PersonService.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Caching.Memory;
 using RickAndMortyAPI.Models;
 using RickAndMortyAPI.Models.ResponseModels;
 using RickAndMortyAPI.Services.Interfaces;
